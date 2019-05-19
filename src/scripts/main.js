cc38f475eff2e64b5130f0026ac8e2aa8c2dd7d5
Alexandr Rybakov <38357811+rybakooov@users.noreply.github.com>
2019-05-19 19:41:16 +0300

    first

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,60 +1,56 @@
 $(document).ready(function(){
 
-  /*spincrement*/
-  var show = true;
-  var countbox = ".advantage-block";
-  $(window).on("scroll load resize", function () {
-      if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
-      var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
-      var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
-      var w_height = $(window).height(); // Высота окна браузера
-      var d_height = $(document).height(); // Высота всего документа
-      var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
-      if (w_top - 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
-          $('.advantage-increment').css('opacity', '1');
-          $('.advantage-increment').spincrement({
-              thousandSeparator: "",
-              duration: 1700
-          });
-            
-          show = false;
-      }
+  /* для пропорциональных блоков*/
+  var koef = 1440/635;
+  $('.main-search-block').height($('.main-search-block').width() / koef);
+  var koef1 = 680/357;
+  $('.main-news-item_b .main-news-item-img').height($('.main-news-item_b .main-news-item-img').width() / koef1);
+  var koef2 = 335/218;
+  $('.main-news-item_s .main-news-item-img').height($('.main-news-item_s .main-news-item-img').width() / koef2);
+  var koef3 = 440/256;
+  $('.head .gen-title').css('left', ($('.head').width() / 2) - ($('.head .gen-title').width() / 2) );
+  var koef4 = 453/200;
+  $('.lots-item__img').height($('.lots-item__img').width() / koef4);
+
+  /* для центрирования заголовка */ 
+  $('.articles-item-img').height($('.articles-item-img').width() / koef3);
+  
+  /* для определения аукционных item*/
+  $('.lots-item').each(function(i, el){
+    if($(el).attr('data-lider') == 1){
+      $(el).find('.lots-item__lider').show();
+    }
+    if($(el).attr('data-favourites') == 1){
+      $(el).find('svg path').css('fill', 'rgba(198,154,96, 1)');
+    }
+  })
+  
+
+  /*resize*/
+  $(window).resize(function(){
+    /* для пропорциональных блоков*/
+    $('.main-search-block').height($('.main-search-block').width() / koef);
+    $('.main-news-item_b .main-news-item-img').height($('.main-news-item_b .main-news-item-img').width() / koef1);
+    $('.main-news-item_s .main-news-item-img').height($('.main-news-item_s .main-news-item-img').width() / koef2);
+    $('.articles-item-img').height($('.articles-item-img').width() / koef3);
+    $('.lots-item__img').height($('.lots-item__img').width() / koef4);
+
+    /* для центрирования заголовка */ 
+    $('.head .gen-title').css('left', ($('.head').width() / 2) - ($('.head .gen-title').width() / 2) );
   });
 
-  /*wow*/
-  new WOW().init();
+  /*меняю стили для хедера на главной*/
+  $('.main-search-block').parent().parent().find('.main-header').addClass('main-header_white');
 
-
-  /*magnific popup*/
-  $('.cta-block__btn').magnificPopup({
-    type: 'inline'
+  /*селектор в шапке*/
+  $('.sort-select-display span').html($('.sort-select-options .sort-opt[data-selected="selected"]').html());
+  $('.sort-select-display').click(function(){
+    $('.sort-select-options').toggleClass('open');
   });
-
-  /*required*/
-  $('.label-wrap').find('[required]').parent().append('<div class="required-dot"></div>');
-  $('.label-wrap textarea').parent().parent().find('.required-dot').css('top', '30px');
-
-  /*mask for form*/
-  $('[name="tel"]').inputmask({
-		mask: "+7 999 999-99-99",
-		showMaskOnHover: false,
-		showMaskOnFocus: true
-  });
-  $('[name="email"]').inputmask({ 
-		alias: "email",
-		showMaskOnHover: false,
-		showMaskOnFocus: true
-  });
-
-
-  /*ticker*/ 
-  $('.ticker').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    speed: 15000,
-    autoplay: true,
-    autoplaySpeed: 0,
-    cssEase: 'linear',
-    arrows: false 
+  $('.sort-opt').click(function(){
+    $('.sort-select-options .sort-opt[data-selected="selected"]').attr('data-selected', '');
+    $(this).attr('data-selected', 'selected');
+    $('.sort-select-options').toggleClass('open');
+    $('.sort-select-display span').html($('.sort-select-options .sort-opt[data-selected="selected"]').html());
   });
 });
